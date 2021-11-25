5a751386ddef25d36599f8edc96f6536d1babb0a
zaid Al-Obeidi <zaid_khalid93@icloud.com>
2021-11-25 11:47:20 -0500

    Anagram method

---
+++ src/JavaR/Methods/AnagramMethod.java
@@ -0,0 +1,26 @@
+package JavaR.Methods;
+
+import java.util.Arrays;
+
+public class AnagramMethod {
+    public static void main(String[] args) {
+        System.out.println("isAnagram(\"listen\",\"silent\") = " + isAnagram("listen", "silent"));
+    }
+
+
+    public static boolean isAnagram(String str1, String str2){
+        //take each one of them and sort it
+        //we only know how to sort in arrays
+        // so we have to get array and sort each character of the words to compare
+
+        char[] ch1 =str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        boolean isAnagram = Arrays.equals(ch1,ch2);
+        return isAnagram;
+    }
+
+
+
+}
