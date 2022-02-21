bbc6e4590221bf5d77d18798e1b0c0e325efd325
zaid Al-Obeidi <zaid_khalid93@icloud.com>
2022-02-21 12:10:05 -0500

    moveZeroes

--- src/InterviewCodingTasks/Array_MoveAllZerosToTheEnd.java
+++ src/InterviewCodingTasks/Array_MoveAllZerosToTheEnd.java
@@ -1,12 +1,26 @@
 package InterviewCodingTasks;
 
+import java.util.Arrays;
+
 public class Array_MoveAllZerosToTheEnd {
 
+    public static void main(String[] args) {
+
+
+        int arr [] = {0,0,0,2,312,3124,123,0,30,0};
+        System.out.println("Arrays.toString(moveZerosToTheEnd(arr)) = "
+                + Arrays.toString(moveZerosToTheEnd(arr)));
+
+        System.out.println("Arrays.toString(moveZeroes(arr)) = "
+                + Arrays.toString(moveZeroes(arr)));
+
+
+    }
     /*
     write a program that can move all the zeros to the end of an array
      */
 
-    public int[] moveZerosToTheEnd(int[] array) {
+    public static int[] moveZerosToTheEnd(int[] array) {
 
         int[] result = new int[array.length];
         int count = 0;
@@ -19,6 +33,20 @@ public class Array_MoveAllZerosToTheEnd {
         return result;
 
     }
+
+
+    public static int[] moveZeroes(int[] arr){
+        int res [] = new int[arr.length];
+        // in ascending order from 0 to ------- etc
+        Arrays.sort(arr);
+        int i = 0 ;
+        for (int each : arr) {
+            // i need a condition
+            if (each != 0)
+            res[i++]=each;
+
+        }
+    return res;}
 
 
 }
