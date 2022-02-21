bdfc8c840774b8e1acdb236f6bc0a8212b6c4687
zaid Al-Obeidi <zaid_khalid93@icloud.com>
2022-02-21 13:12:17 -0500

    Reverse sortingArrayAsc sortingArrayDsc

--- src/InterviewCodingTasks/Array_SortInAscendingOrder.java
+++ src/InterviewCodingTasks/Array_SortInAscendingOrder.java
@@ -3,13 +3,21 @@ package InterviewCodingTasks;
 import java.util.Arrays;
 
 public class Array_SortInAscendingOrder {
+    public static void main(String[] args) {
+        int arr [] = {14,141,51,51,51515,5215214,4124124,2,4};
+        System.out.println(Arrays.toString(sortingArrayAsc(arr)));
+        System.out.println(Arrays.toString(sortingArray(arr)));
+
+    }
 
     /*
     Write a method that can sort an int array in Ascending order without using the sort method
      */
 
-    public int[] sortingArrayAsc(int[] arr) {
+    public static int[] sortingArrayAsc(int[] arr) {
 
+//        The java.util.Arrays.copyOfRange(short[] original, int from, int to)
+//        method copies the specified range of the specified array into a new array
         int[] result = Arrays.copyOfRange(arr, 0, arr.length);
 
         for (int i = 0; i < result.length; i++) {
@@ -28,5 +36,15 @@ public class Array_SortInAscendingOrder {
 
     }
 
+    //my way
+    public static int[] sortingArray(int[] arr) {
+        Arrays.sort(arr);
+        int[] res = new int[arr.length];
+        for (int i = 0,j=0; i < arr.length;j++, i++) {
+            res[j]= arr[i];
+        }
+        return res;
+    }
 
-}
+
+    }
--- src/InterviewCodingTasks/Array_SortInDescendingOrder.java
+++ src/InterviewCodingTasks/Array_SortInDescendingOrder.java
@@ -3,12 +3,17 @@ package InterviewCodingTasks;
 import java.util.Arrays;
 
 public class Array_SortInDescendingOrder {
+    public static void main(String[] args) {
+        int arr [] = {14,141,51,51,51515,5215214,4124124,2,4};
+        System.out.println(Arrays.toString(sortingArrayDsc(arr)));
+        System.out.println(Arrays.toString(sortingArray(arr)));
 
+    }
     /*
         Write a return method that can sort an int array in descending order without using the sort method
      */
 
-    public int[] sortingArrayAsc(int[] arr) {
+    public static int[] sortingArrayDsc(int[] arr) {
 
         int[] result = Arrays.copyOfRange(arr, 0, arr.length);
 
@@ -28,5 +33,14 @@ public class Array_SortInDescendingOrder {
 
     }
 
+    // my way
+    public static int[] sortingArray(int[] arr) {
+        Arrays.sort(arr);
+        int[] res = new int[arr.length];
+        for (int i = 0,j=arr.length-1; i < arr.length;j--, i++) {
+            res[j]= arr[i];
+        }
+        return res;
+    }
 
 }
