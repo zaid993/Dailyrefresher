9f77d97fd36aed824e0539006ee5311ed96a426c
zaid Al-Obeidi <zaid_khalid93@icloud.com>
2022-02-21 11:58:41 -0500

    Merge two arrays

--- src/InterviewCodingTasks/Array_MergeTwoArrays.java
+++ src/InterviewCodingTasks/Array_MergeTwoArrays.java
@@ -1,6 +1,17 @@
 package InterviewCodingTasks;
 
+import java.util.Arrays;
+
 public class Array_MergeTwoArrays {
+
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3};
+        int [] arr2 = {4,5,6};
+        System.out.println("Arrays.toString(concatTwoArrays(arr1,arr2)) = "
+                + Arrays.toString(concatTwoArrays(arr1, arr2)));
+    }
+
+
 
     /*
     Write a return method that can concat two arrays
@@ -18,9 +29,24 @@ public class Array_MergeTwoArrays {
         for (int each : arr2) {
             result[i++] = each;
         }
-
         return result;
 
+    }
+
+    public static int[] AddTwo(int[] arr, int[] arr2){
+
+        int[] res = new int[arr.length + arr2.length];
+
+        int i = 0;
+        //without increment it won't work so create a var for iteration 👆🏻
+        for (int each : arr) {
+            res[i++] = each;
+        }
+        for (int each2 : arr2) {
+            res[i++]=each2;
+        }
+
+        return res;
     }
 
 }
